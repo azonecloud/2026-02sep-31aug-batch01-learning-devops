7ec1222b67e0af1a9955e9958932ed0074f20ca0
azonecloud <100863730+azonecloud@users.noreply.github.com>
2026-08-31 16:36:18 +0530

    Latest Commits

--- 00-sources/pythonapp/app.py
+++ 00-sources/pythonapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker + Kubernetes! for V2.0"
+    return "Hello from Docker + Kubernetes! for V3.0....By Kumaran, DevOps Engineer"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
