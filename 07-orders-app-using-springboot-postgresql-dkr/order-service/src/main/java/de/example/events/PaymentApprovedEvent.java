46f4de2970705d66df75ca74f18ca54a102772cb
azonecloud <100863730+azonecloud@users.noreply.github.com>
2026-09-01 22:13:05 +0530

    Latest Commits

--- 07-orders-app-using-springboot-postgresql-dkr/order-service/src/main/java/de/example/events/PaymentApprovedEvent.java
+++
@@ -1,8 +0,0 @@
-package de.example.events;
-
-import java.time.Instant;
-import java.util.UUID;
-
-public record PaymentApprovedEvent(UUID orderId, Instant timestamp) implements PaymentResultEvent {
-
-}
