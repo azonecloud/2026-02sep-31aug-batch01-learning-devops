fd0f99a3c306701c4b5e80ae4cc6055bb8c3f7b5
azonecloud <100863730+azonecloud@users.noreply.github.com>
2026-09-02 17:01:08 +0530

    Latest Commits

--- 01-using-terraform/s3-bucket.tf
+++ 01-using-terraform/s3-bucket.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-
+  bucket = <<PLACE_WHAT_EVER_NAME_YOU_WANT_TO_USE_FOR_YOUR_BUCKET>>
+  #bucket = var.bucket_name
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
